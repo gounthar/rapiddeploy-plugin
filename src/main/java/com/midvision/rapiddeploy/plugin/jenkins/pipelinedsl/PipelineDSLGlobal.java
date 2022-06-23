5079c72c4e6acb546d15a70c764fc9516b996823
rafaelruizpalacios <rafaelruizpalacios@localhost>
2022-06-23 10:23:11 +0200

    Fix for SECURITY-359 / CVE-2022-30945 (internal #3968)

--- src/main/java/com/midvision/rapiddeploy/plugin/jenkins/pipelinedsl/PipelineDSLGlobal.java
+++ src/main/java/com/midvision/rapiddeploy/plugin/jenkins/pipelinedsl/PipelineDSLGlobal.java
@@ -6,40 +6,52 @@ import java.io.Reader;
 import org.jenkinsci.plugins.workflow.cps.CpsScript;
 import org.jenkinsci.plugins.workflow.cps.CpsThread;
 import org.jenkinsci.plugins.workflow.cps.GlobalVariable;
+import org.jenkinsci.plugins.workflow.cps.GroovySourceFileAllowlist;
 
 import groovy.lang.Binding;
 import groovy.lang.GroovyCodeSource;
+import hudson.Extension;
 
 public abstract class PipelineDSLGlobal extends GlobalVariable {
 
-	public abstract String getFunctionName();
+    public abstract String getFunctionName();
 
-	@Override
-	public String getName() {
-		return getFunctionName();
-	}
+    @Override
+    public String getName() {
+        return getFunctionName();
+    }
 
-	@Override
-	public Object getValue(final CpsScript script) throws Exception {
-		final Binding binding = script.getBinding();
+    @Override
+    public Object getValue(final CpsScript script) throws Exception {
+        final Binding binding = script.getBinding();
 
-		final CpsThread c = CpsThread.current();
-		if (c == null) {
-			throw new IllegalStateException("Expected to be called from CpsThread");
-		}
+        final CpsThread c = CpsThread.current();
+        if (c == null) {
+            throw new IllegalStateException("Expected to be called from CpsThread");
+        }
 
-		final ClassLoader cl = getClass().getClassLoader();
+        final ClassLoader cl = getClass().getClassLoader();
 
-		final String scriptPath = "dsl/" + getFunctionName() + ".groovy";
-		final Reader r = new InputStreamReader(cl.getResourceAsStream(scriptPath), "UTF-8");
+        final String scriptPath = "dsl/" + getFunctionName() + ".groovy";
+        final Reader r = new InputStreamReader(cl.getResourceAsStream(scriptPath), "UTF-8");
 
-		final GroovyCodeSource gsc = new GroovyCodeSource(r, getFunctionName() + ".groovy", cl.getResource(scriptPath).getFile());
-		gsc.setCachable(true);
+        final GroovyCodeSource gsc = new GroovyCodeSource(r, getFunctionName() + ".groovy", cl.getResource(scriptPath).getFile());
+        gsc.setCachable(true);
 
-		final Object pipelineDSL = c.getExecution().getShell().getClassLoader().parseClass(gsc).newInstance();
-		binding.setVariable(getName(), pipelineDSL);
-		r.close();
+        final Object pipelineDSL = c.getExecution().getShell().getClassLoader().parseClass(gsc).newInstance();
+        binding.setVariable(getName(), pipelineDSL);
+        r.close();
 
-		return pipelineDSL;
-	}
+        return pipelineDSL;
+    }
+
+    @Extension
+    public static class RapidDeployConnectorAllowlist extends GroovySourceFileAllowlist {
+        private final String scriptUrl = PipelineDSLGlobal.class.getResource("/dsl/RdConnectorProxy.groovy").toString();
+
+        @Override
+        public boolean isAllowed(final String groovyResourceUrl) {
+            return groovyResourceUrl.equals(scriptUrl);
+        }
+    }
 }
